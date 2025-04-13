a6fe7f47e7048a52dcee5232ee2fa6e73c4dfa17
shivam0897-i <123145276+shivam0897-i@users.noreply.github.com>
2025-04-13 15:56:23 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './app.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
 /// <reference types="vite/client" />
